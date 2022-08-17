d380ce465af138e2deaebce067d419f1eea0386c
Francisco <franciscoldinucci@gmail.com>
2022-08-17 19:55:49 -0300

    homework1.2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -75,7 +75,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.length == str2.length) ¨{
+  if (str1.length == str2.length) {
     return true;
   }
   else return false;
