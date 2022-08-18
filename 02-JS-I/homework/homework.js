5a2e35ce8adb98de3bcee3860cb19b0545df4b40
Francisco <franciscoldinucci@gmail.com>
2022-08-17 21:48:08 -0300

    homework1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -181,7 +181,8 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  console.log(str+"!");
+  simbolo = console.log(str+"!");
+  return simbolo;
 }
 
 function combinarNombres(nombre, apellido) {
