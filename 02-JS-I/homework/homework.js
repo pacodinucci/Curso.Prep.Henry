0c83f4cf09136d64e791a5d2932bc3298dd30482
Francisco <franciscoldinucci@gmail.com>
2022-08-17 22:19:17 -0300

    homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -162,7 +162,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  return Math.numeroRandom();
+  return Math.random();
   
 }
 
