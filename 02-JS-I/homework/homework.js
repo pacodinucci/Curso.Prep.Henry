c1d35e4da946ec58512c33f5ad3b488d5b18b2e1
Francisco <franciscoldinucci@gmail.com>
2022-08-17 21:53:58 -0300

    homework1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -185,6 +185,8 @@ function agregarSimboloExclamacion(str) {
   return simbolo;
 }
 
+agregarSimboloExclamacion("hello world");
+
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
