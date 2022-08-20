4c9f20860f2496c231da96b2f6200936a51624e2
Francisco <franciscoldinucci@gmail.com>
2022-08-20 20:48:44 -0300

    finish homework 4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -52,6 +52,8 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto[unaPropiedad];
+  return objeto;
 }
 
 function nuevoUsuario(nombre, email, password) {
