df5b175c9d9e555c58171bda3884c5d9598c5953
Francisco <franciscoldinucci@gmail.com>
2022-08-19 22:08:22 -0300

    homework3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -165,6 +165,9 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  arreglo2 = new Set(arreglo);
+  final = [...arreglo2];
+  return final.length;
   
 } 
 
