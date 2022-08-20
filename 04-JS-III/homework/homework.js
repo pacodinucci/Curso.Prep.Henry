07262efd1359cc884f995cd0dc2b17b86ebb347b
Francisco <franciscoldinucci@gmail.com>
2022-08-19 21:00:17 -0300

    homework3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -130,6 +130,14 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  cantidad = [];
+  for (i=0;i<arreglo.length;i++) {
+    if (arreglo[i]>18) {
+      cantidad.push(arreglo[i]);
+    }
+  return cantidad;  
+  }
+
 
 }
 
