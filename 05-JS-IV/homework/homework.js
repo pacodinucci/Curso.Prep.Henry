e104c465483ac1666dd1cf1570b660e23ba2912d
Francisco <franciscoldinucci@gmail.com>
2022-08-20 22:33:22 -0300

    finish homework 4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -160,7 +160,7 @@ function agregarMetodoCalculoDescuento(producto) {
   producto.calcularPrecioDescuento = function () {
     return this.precio - (this.precio * this.porcentajeDescuento);
   }
-  return this.producto;
+  return producto;
 
 }
 
