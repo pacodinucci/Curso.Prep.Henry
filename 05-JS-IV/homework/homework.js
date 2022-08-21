087d36b1b7df66ef650dc31b5e3580edff539e5f
Francisco <franciscoldinucci@gmail.com>
2022-08-20 23:59:30 -0300

    homework4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -169,6 +169,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  function calcularPrecioDescuento() {
+    this.precio - (this.precio * this.porcentajeDeDescuento);
+  }
+  return producto;
  
 }
 
