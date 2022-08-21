83a658c1fc0bd6ae5a04ed6fea4cbf378839f1d9
Francisco <franciscoldinucci@gmail.com>
2022-08-21 18:11:50 -0300

    homework5

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -9,10 +9,10 @@ function crearUsuario() {
   // Devuelve la clase
   // Tu código:
   function Usuario(opciones) {
-    this.usuario: opciones.usuario,
-    this.nombre: opciones.nombre,
-    this.email: opciones.email,
-    this.password: opciones.password
+    this.usuario = opciones.usuario,
+    this.nombre = opciones.nombre,
+    this.email = opciones.email,
+    this.password = opciones.password
   }
 
   Usuario.prototype.saludar = function () {
