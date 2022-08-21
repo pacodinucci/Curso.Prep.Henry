73cdf5f7aa21db4d29e9aa50dcece17e49cb7f97
Francisco <franciscoldinucci@gmail.com>
2022-08-20 23:29:45 -0300

    homework4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -117,6 +117,8 @@ function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword;
+  return usuario;
   
 }
 
