22d3b4eeed969e3cf39a1a8f22366077965bfa1c
Francisco <franciscoldinucci@gmail.com>
2022-08-20 23:09:07 -0300

    homework4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -64,6 +64,12 @@ function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  usuario = {
+    nombre: nombre,
+    email: email,
+    password: password
+  }
+  return objeto;
   
 
 }
