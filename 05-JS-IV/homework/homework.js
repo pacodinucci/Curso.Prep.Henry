485334cb7b6c69e4ccca6810af773075be87fa5f
Francisco <franciscoldinucci@gmail.com>
2022-08-20 23:57:07 -0300

    homework4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -154,7 +154,7 @@ function sumarLikesDeUsuario(usuario) {
   // Tu código:
   suma = 0;
   for (var i = 0;i<usuario.posts.length;i++) {
-    suma = suma + post[i].likes;
+    suma = suma + usuario.posts[i].likes;
   }
   return suma;
 }
