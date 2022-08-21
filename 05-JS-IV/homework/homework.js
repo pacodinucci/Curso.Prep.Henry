ee6785f4aaa7e01865cc2a1e1e510169e8405a36
Francisco <franciscoldinucci@gmail.com>
2022-08-20 23:10:37 -0300

    homework4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -78,6 +78,10 @@ function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if (usuario[email]) {
+    return true;
+  }
+  return false;
   
   
 }
