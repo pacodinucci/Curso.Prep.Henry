f2a2da2231602eb0ef077acae1e056f1de5bef66
Francisco <franciscoldinucci@gmail.com>
2022-08-21 18:16:53 -0300

    homework5

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -29,6 +29,9 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function () {
+    return 'Hello World!!';
+  }
 }
 
 function agregarStringInvertida() {
