ef7583a465fffb170a61c23404416e4fc6be55bc
Francisco <franciscoldinucci@gmail.com>
2022-08-20 23:17:47 -0300

    homework4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -93,6 +93,10 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (objeto[propiedad]) {
+    return true;
+  }
+  return false;
   
 }
 
