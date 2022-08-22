eec2ac0610c9ede60737159906ef08e9f5a3d44b
Francisco <franciscoldinucci@gmail.com>
2022-08-21 21:52:51 -0300

    finish homework 6

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -17,6 +17,7 @@ function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
