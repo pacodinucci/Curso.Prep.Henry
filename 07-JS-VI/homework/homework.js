d95c142e041b15aa5a72c7a1fbb47119e83560be
Francisco <franciscoldinucci@gmail.com>
2022-08-21 23:18:30 -0300

    finish homework 6

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -35,7 +35,7 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  array.forEach(cb(elemento) {
+  array.forEach(function (elemento) {
     console.log(elemento);
   });
 }
